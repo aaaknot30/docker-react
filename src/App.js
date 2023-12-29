78edd0c4a3124de5224697d99183d26f9dd6effe
Kyle Larson <aaaknot30@yahoo.com>
2023-12-28 22:11:49 -0700

    “deploy-6

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Let's go Rock Climbing!</h1>
+        <h1>Let's go Jet Skiing!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
